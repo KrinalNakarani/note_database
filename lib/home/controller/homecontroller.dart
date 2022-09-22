061c6870a3d90dcd53442188451c9b93961018d7
KrinalNakarani <krinalnakarani1774@gmail.com>
2022-09-22 10:19:19 +0530

    22-9-2022

--- lib/home/controller/homecontroller.dart
+++ lib/home/controller/homecontroller.dart
@@ -1,1 +1,34 @@
-class
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Gcantroller extends GetxController {
+  void AddData({String? id, String? task}) {
+    var firebaseDatabase = FirebaseDatabase.instance;
+    var FDBref = firebaseDatabase.ref();
+
+    if (key == null) {
+      FDBref.child("Task").push().set({"id": id, "task": task});
+    } else {
+      FDBref.child("Task").child(key).set({"id": id, "task": task});
+    }
+  }
+
+  Stream<DatabaseEvent> readData() {
+    var firebaseDatabase = FirebaseDatabase.instance;
+    var FDBref = firebaseDatabase.ref();
+    return FDBref.child("Task").onValue;
+  }
+
+  void Delete(String? key) {
+    var firebaseDatabase = FirebaseDatabase.instance;
+    var FDBref = firebaseDatabase.ref();
+    FDBref.child("Task").child(key!).remove();
+  }
+}
+
+class ModalNote {
+  String? id,task,key;
+
+  ModalNote({this.id, this.task,this.key});
+}
--- lib/home/view/homePage.dart
+++ lib/home/view/homePage.dart
@@ -1,5 +1,10 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../controller/homecontroller.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,12 +14,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Gcantroller GLR = Get.put(Gcantroller());
+  TextEditingController id = TextEditingController();
+  TextEditingController task = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(),
+        body: StreamBuilder(
+          stream: GLR.readData(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            } else if (snapshot.hasData) {
+              print("====================${snapshot.data.snapshot}");
+
+              List l1 = [];
+              DataSnapshot data = snapshot.data.snapshot;
+
+              for (var x in data.children) {
+                ModalNote N1 = ModalNote(
+                    id: x.child("id").value.toString(),
+                    task: x.child("task").value.toString(),
+                    key: x.key);
+                l1.add(N1);
+              }
+              return ListView.builder(
+                  itemCount: l1.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text("${l1[index].task}"),
+                      trailing: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              id = TextEditingController(text: l1[index].id);
+                              task =
+                                  TextEditingController(text: l1[index].task);
+
+                              // DialogBox(l1[index].key.toString());
+                            },
+                            icon: Icon(
+                              Icons.edit,
+                              color: Colors.green,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              GLR.Delete(l1[index].key);
+                            },
+                            icon: Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            }
+            return Center(child: CircularProgressIndicator());
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+
+        ),
       ),
     );
   }
+
+  void Tick() {}
 }
